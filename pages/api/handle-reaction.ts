ce9defec614d462960fb376d397ae92a20a88cfa
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-06-04 22:37:16 +0900

    POSTリクエスト時にchallengeを返すように加筆

--- pages/api/handle-reaction.ts
+++ pages/api/handle-reaction.ts
@@ -1,9 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log(req)
-  res.status(200)
+  // console.log(req)
+  if (req.method === 'POST') {
+    console.log(req.body.challenge)
+    res.status(200).json({ challenge: req.body.challenge })
+  } else {
+    res.status(200)
+  }
 }
